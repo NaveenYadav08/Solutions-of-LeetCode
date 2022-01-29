4d2822f5f31d48c726cf4030496409215ce0b6f0
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-01-30 03:32:23 +0530

    Update Left Right Bottom Top Views ( BFS & DFS ) .cpp

--- BINARY TREE AND BINARY SEARCH TREE/Left Right Bottom Top Views ( BFS & DFS ) .cpp
+++ BINARY TREE AND BINARY SEARCH TREE/Left Right Bottom Top Views ( BFS & DFS ) .cpp
@@ -44,4 +44,98 @@ DFS -
 
 
 TOP and BOTTOM VIEW 
+BFS 
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        vector<int> ans;
+        
+        // base case
+        if(root == NULL){
+            return ans;
+        }
+    
+        Node *temp = NULL;
+        //creating empty queue for level order traversal.
+        queue<pair<Node *, int>> q;
+        //creating a map to store nodes at a particular horizontal distance.
+        map<int, int> mp;
+    
+        q.push({root, 0});
+        while(!q.empty())
+        {
+    
+            temp = q.front().first;
+            int d = q.front().second;
+            q.pop();
+            
+            //storing temp->data in map.
+            if(mp.find(d) == mp.end()){
+                mp[d] = temp->data;
+            }
+                //  IN CASE OF BOTTOM THIS CONDITION WILL NOT COME 
+                //  ONLY THIS 
+                 // mp[d] = temp->data;
+            
+            //if left child of temp exists, pushing it in
+            //the queue with the horizontal distance.
+            if(temp->left){
+                q.push({temp->left, d-1});
+            }
+            //if right child of temp exists, pushing it in
+            //the queue with the horizontal distance.
+            if(temp->right){
+                q.push({temp->right, d+1});
+            }
+        }
+        //traversing the map and storing the nodes in list 
+        //at every horizontal distance.
+        for(auto it = mp.begin(); it != mp.end(); it++){
+            ans.push_back(it->second);
+        }
+        //returning the output list.
+        return ans;
+    }
+};
 
+
+
+DFS 
+void fillMap(Node* root, int d, int l,
+             map<int, pair<int, int> >& m)
+{
+    if (root == NULL)
+        return;
+ 
+    if (m.count(d) == 0) {
+        m[d] = make_pair(root->data, l);
+    }
+    else if (m[d].second > l) {
+        m[d] = make_pair(root->data, l);
+    }
+        // IN CASE OF BOTTOM VIEW else if condition change 
+        //  else if (m[d].second <= l) {
+       
+ 
+    fillMap(root->left, d - 1, l + 1, m);
+    fillMap(root->right, d + 1, l + 1, m);
+}
+
+void topView(struct Node* root)
+{
+ 
+    // map to store the pair of node value and its level
+    // with respect to the vertical distance from root.
+    map<int, pair<int, int> > m;
+ 
+    // fillmap(root,vectical_distance_from_root,level_of_node,map)
+    fillMap(root, 0, 0, m);
+ 
+    for (auto it = m.begin(); it != m.end(); it++) {
+        cout << it->second.first << " ";
+    }
+}
