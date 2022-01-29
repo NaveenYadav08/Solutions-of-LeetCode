4a27b2d04939351c5dabede6972b41563fc8919b
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-01-29 23:17:30 +0530

    Update PREORDER ( Iterative) .cpp

--- BINARY TREE AND BINARY SEARCH TREE/PREORDER ( Iterative) .cpp
+++ BINARY TREE AND BINARY SEARCH TREE/PREORDER ( Iterative) .cpp
@@ -6,10 +6,13 @@
 ██║░░██║███████╗╚██████╔╝╚█████╔╝██║░░██║██║░░░██║░░░██║░░██║██║░╚═╝░██║
 ╚═╝░░╚═╝╚══════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝
 
-
-
-
-
+1) Create an empty stack nodeStack and push root node to stack. 
+2) Do the following while nodeStack is not empty. 
+….a) Pop an item from the stack and print it. 
+….b) Push right child of a popped item to stack 
+….c) Push left child of a popped item to stack
+The right child is pushed before the left child to make sure that the left subtree is processed first.
+  
 
 
 ░█████╗░░█████╗░██████╗░███████╗
@@ -18,3 +21,37 @@
 ██║░░██╗██║░░██║██║░░██║██╔══╝░░
 ╚█████╔╝╚█████╔╝██████╔╝███████╗
 ░╚════╝░░╚════╝░╚═════╝░╚══════╝
+
+void preorderIterative(Node* root)
+{
+    if (root == NULL)
+        return;
+ 
+    stack<Node*> st;
+ 
+    // start from root node (set current node to root node)
+    Node* curr = root;
+ 
+    // run till stack is not empty or current is
+    // not NULL
+    while (!st.empty() || curr != NULL) {
+        // Print left children while exist
+        // and keep pushing right into the
+        // stack.
+        while (curr != NULL) {
+            cout << curr->data << " ";
+ 
+            if (curr->right)
+                st.push(curr->right);
+ 
+            curr = curr->left;
+        }
+ 
+        // We reach when curr is NULL, so We
+        // take out a right child from stack
+        if (st.empty() == false) {
+            curr = st.top();
+            st.pop();
+        }
+    }
+}
