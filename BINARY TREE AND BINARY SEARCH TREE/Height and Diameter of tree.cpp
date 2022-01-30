25e5094e92686515c1ef734faef3e1fb43d4d566
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-01-30 17:04:03 +0530

    Update Height and Diameter of tree.cpp

--- BINARY TREE AND BINARY SEARCH TREE/Height and Diameter of tree.cpp
+++ BINARY TREE AND BINARY SEARCH TREE/Height and Diameter of tree.cpp
@@ -15,6 +15,20 @@
     } 
 
 
+height OF N-ARY TREE
+
+    int maxDepth(Node* root) {
+        if(root==NULL) return 0;
+        
+        int MAXI=0;
+        for(int i=0;i<root->children.size();i++)
+        {
+            MAXI=max(MAXI,maxDepth(root->children[i]));
+        }
+        return MAXI+1;
+        
+    }
+
 █▀▄ █ ▄▀█ █▀▄▀█ █▀▀ ▀█▀ █▀▀ █▀█
 █▄▀ █ █▀█ █░▀░█ ██▄ ░█░ ██▄ █▀▄
 
