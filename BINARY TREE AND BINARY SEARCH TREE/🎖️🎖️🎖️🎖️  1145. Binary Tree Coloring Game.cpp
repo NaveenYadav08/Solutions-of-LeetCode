1c8dbc5a833a7131826bc71d47837991a4a66769
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-02-06 00:43:50 +0530

    Create 🎖️🎖️🎖️🎖️  1145. Binary Tree Coloring Game.cpp

---
+++ BINARY TREE AND BINARY SEARCH TREE/🎖️🎖️🎖️🎖️  1145. Binary Tree Coloring Game.cpp
@@ -0,0 +1,40 @@
+LEETCODE - https://leetcode.com/problems/binary-tree-coloring-game/
+
+
+█▀▀ █▀█ █▀▄ █▀▀
+█▄▄ █▄█ █▄▀ ██▄
+
+
+class Solution {
+public:
+    
+    static void countNodes(TreeNode* root,int& ans){
+        if(root==NULL){
+            return;
+        }
+        countNodes(root->left,ans);
+        countNodes(root->right,ans);
+        ans=ans+1;
+    }
+    
+    static void findX(TreeNode* root,int x,int& ans1,int& ans2){
+        if(root==NULL){
+            return;
+        }
+        if(root->val==x){
+            countNodes(root->left,ans1);
+            countNodes(root->right,ans2);
+            return;
+        }
+        findX(root->left,x,ans1,ans2);
+        findX(root->right,x,ans1,ans2);
+    }
+    
+    bool btreeGameWinningMove(TreeNode* root, int n, int x) {
+        int ans1,ans2,ans3;
+        ans1=ans2=ans3=0;
+        findX(root,x,ans1,ans2);
+        ans3=n-ans1-ans2-1;
+        return (max(ans1,max(ans2,ans3)) > n/2);
+    }
+};
