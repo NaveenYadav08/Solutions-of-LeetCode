26633623fddac057f4032ef4d04d0454f00c3cb5
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-04-03 23:42:19 +0530

    Create 🎖️ 61. Rotate List.cpp

---
+++ LINKED LISTS/🎖️ 61. Rotate List.cpp
@@ -0,0 +1,31 @@
+https://leetcode.com/problems/rotate-list/
+
+// code 
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        // edge cases 
+        if (!head || !head->next || k == 0) return head;
+        
+        // compute the length
+        ListNode *cur = head;
+        int len = 1;
+        while (cur->next && ++len) 
+            cur = cur->next;
+        
+        // go till that node
+        cur->next = head;
+        k = k % len; 
+        k = len - k;
+        while (k--) cur = cur->next;
+        cout<<cur->val<<endl;
+        
+        
+        // make the node head and break connection 
+        head = cur->next;
+        cur->next = NULL;
+        
+        
+        return head; 
+    }
+};
