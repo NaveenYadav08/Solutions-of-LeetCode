7986f0da7ea6fe9ad586d8aef32116b18451c475
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-04-03 19:53:38 +0530

    Update 19. Remove Nth Node From End of List.cpp

--- LINKED LISTS/19. Remove Nth Node From End of List.cpp
+++ LINKED LISTS/19. Remove Nth Node From End of List.cpp
@@ -1,1 +1,33 @@
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 
+
+// make diff of n bw slow and fast
+// 1 2 3 4 5 6 , n = 2    slow 1,  fast = 3
+// now move till fast next is NULL
+// at this point slow is behind the element I will delete 
+
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+        ListNode* slow=head;
+        ListNode* fast=head;
+        for(int i=0;i<n; i++)
+        {
+            fast=fast->next;
+        }
+        
+         if(!fast) return head->next;
+        
+        while(fast->next)
+        {
+            fast=fast->next;
+            slow=slow->next;
+        }
+        
+        slow->next=slow->next->next;
+        return head;
+    }
+
+};
