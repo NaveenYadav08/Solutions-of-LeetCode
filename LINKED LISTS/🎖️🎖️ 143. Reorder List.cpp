d62c0aad6ea6b8ae216d4906b7d4413caa049b0c
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-04-03 21:14:12 +0530

    Create 🎖️🎖️ 143. Reorder List.cpp

---
+++ LINKED LISTS/🎖️🎖️ 143. Reorder List.cpp
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/reorder-list/
+
+//CODE
+
+class Solution {
+public:
+void reorderList(ListNode* head) {
+        stack<ListNode *> s;
+        ListNode* p = head;
+        while(p){
+            s.push(p);
+            p = p->next;
+        }
+        int num_nodes = s.size();
+        while(s.size() > num_nodes / 2 + 1){
+            if(!head->next){
+                break;
+            }
+            ListNode * temp = head->next;
+            head->next = s.top();
+            s.top()->next = temp;
+            head = temp;
+            s.pop();
+        }
+        s.top()->next = NULL;
+    }
+};
