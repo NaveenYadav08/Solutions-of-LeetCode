5c645b7488a8f427e744858d15f4059e387bcf0a
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-04-30 10:28:20 +0530

    Create 3 Graph BFS.cpp

---
+++ GRAPHS/BASICS/3 Graph BFS.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//BFS
+
+// class Graph
+// GENERIC GRAPH
+template<typename T>
+
+class Graph{
+    //int V;
+map<T,list<T>> l;
+
+    public:
+    // Graph(int V)
+    // {
+    //     this->V=V;
+    //     l = new list<int>[V];
+    // }
+
+    void addedge(int x, int y)
+    {
+        l[x].push_back(y);
+        l[y].push_back(x);
+
+    }
+
+    void bfs(T src)
+    {
+        // queue and vis array
+        map<T,int> visited;
+        queue<T> q;
+        q.push(src);
+        visited[src]=true;
+        
+
+        while(!q.empty())
+        {
+            T node = q.front();
+            q.pop();
+            
+       
+            cout<< node<<"  ";
+            for(int nbr:l[node])
+            {
+                if(!visited[nbr])
+                {
+                    q.push(nbr);
+                    visited[nbr]=true;
+                }
+            }
+            
+
+        }
+         
+    }
+
+
+
+};
+
+
+int main() {
+    // cout<<"Hello World!";
+    Graph<int> g;
+    g.addedge(0,1);
+    g.addedge(1,2);
+    g.addedge(2,3);
+    g.addedge(3,4);
+    g.addedge(4,5);
+    g.bfs(0);
+    return 0;
+}
