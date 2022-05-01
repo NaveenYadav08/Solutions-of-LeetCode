a35e9fecb384aaf90b60ff8dfb9362e4b1907f35
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-01 19:32:29 +0530

    Create 16 CYCLE DETECTOR DFS DIRECTED GRAPH .cpp

---
+++ GRAPHS/BASICS/16 CYCLE DETECTOR DFS DIRECTED GRAPH .cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
+    list<int> *l;
+    int V;
+    public:
+    Graph(int V)
+    {
+        this->V=V;
+        l=new list<int>[V];
+    }
+
+void addEdge(int x, int y, bool directed = true)
+{
+    l[x].push_back(y); 
+    if(!directed)
+    {
+        l[y].push_back(x);
+    }
+}
+
+bool cycle_helper( int node, bool *visited, bool *arrayy)
+{
+    visited[node]=true;
+    arrayy[node]=true;
+    for(auto nbr:l[node])
+    {
+        //two cases
+        if(!visited[nbr])
+        { // go and recursively visit neighbour
+         bool cycle_mila = cycle_helper(nbr,visited,arrayy);
+         if(cycle_mila)
+         { return true;
+
+         }
+           
+
+        }
+        // nbr visit and part of current path
+        else if(arrayy[nbr]==true)
+        {
+           return true;
+        }
+    }
+    // leave a node 
+    arrayy[node]=false;
+   return false;
+}
+
+bool contains_cycle()
+{// CHeck for cycle in Directed Graph
+     bool *visited =new bool[V];
+     bool *arrayy = new bool[V];
+     for( int i=0; i<V; i++ )
+     {
+         visited[i]=false;
+         arrayy[i]=false;
+
+     }
+    return cycle_helper(0,visited,arrayy);
+
+}
+
+};
+
+int main() {
+    Graph g(7);
+    g.addEdge(0,1);
+     g.addEdge(1,2);
+      g.addEdge(2,3);
+       g.addEdge(3,4);
+        g.addEdge(4,5);
+         g.addEdge(1,5);
+          g.addEdge(5,6);
+           g.addEdge(4,2); // Backedge
+
+
+        if(g.contains_cycle())
+        {
+           cout<<"YES"<<endl;
+        }
+        else
+        cout<<"NO"<<endl;
+
+}
