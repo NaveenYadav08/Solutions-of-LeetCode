d93b169edc3160369093a4f7490b7812a88cd407
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-01 11:34:49 +0530

    Create 14 TOPOLOGICAL SORT DFS .cpp

---
+++ GRAPHS/BASICS/14 TOPOLOGICAL SORT DFS .cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class Graph{
+    map<T,list<T>> l;
+    public:
+    void addEdge(string x,string y)
+    {
+        l[x].push_back(y);
+        // l[y].push_back(x);
+    }
+
+    void dfs_helper(T src,map<T,bool> &visited,  list<T> &ordering)
+    {
+     // it teaverse the graph
+     //cout<<src<<" "<<endl;
+     visited[src]=true;
+     for(auto x:l[src])
+     {
+         if(!visited[x])
+         {
+             dfs_helper(x,visited,ordering); 
+         }
+     }
+     ordering.push_front(src);
+     return;
+    }
+
+    void dfs()
+    {
+     
+     map<T,bool> visited;
+     list<T> ordering;
+
+     for(auto x:l)
+     {
+         T node=x.first;
+         visited[node]=false;
+     }
+
+     for(auto p:l)
+     {  T node=p.first;
+            if(!visited[node])
+            {
+         dfs_helper(node,visited,ordering);
+            }
+     }
+
+
+     //finally  print list
+     for(auto node:ordering)
+     cout<<node<<endl;
+
+     
+    }
+};
+
+int main() {
+    Graph<string> g;
+    g.addEdge("python","DataPreprocessing");
+     g.addEdge("python","pyTorch");
+      g.addEdge("python","ML");
+        g.addEdge("DataPreprocessing","ML");
+         g.addEdge("pyTorch","DL");
+          g.addEdge("ML","DL");
+           g.addEdge("DL","faceRecogn");
+            g.addEdge("Dataset","faceRecogn");
+         
+    
+    g.dfs();
+}
