5f6f60ae191dda45d7f64f1029de599a5ff542da
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-09 10:21:17 +0530

    Update 🎖️ 459. Repeated Substring Pattern.cpp

--- STRINGS/🎖️ 459. Repeated Substring Pattern.cpp
+++ STRINGS/🎖️ 459. Repeated Substring Pattern.cpp
@@ -1,2 +1,43 @@
 // link https://leetcode.com/problems/repeated-substring-pattern/
 
+// lps nikala 
+// now lps[n-1] = longest suffix also prefic 
+// so n - lps[n-1] = len of repeatng part 
+
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        
+        int n=s.length();
+        vector<int> dp(n,0);
+        
+        int i=1;
+        int len=0;
+        
+        for(i=1;i<n;)
+        {
+            if(s[i]==s[len])
+            {
+                len++;
+                dp[i]=len;
+                i++;
+            }
+            else
+            {
+                if(len!=0)
+                {
+                    len=dp[len-1];
+                }
+                else
+                {
+                    dp[i]=0;
+                    i++;
+                }
+            }
+        }
+        
+        // lps computed
+        
+        return dp[n-1] && (n%(n-dp[n-1])==0);
+    }
+};
