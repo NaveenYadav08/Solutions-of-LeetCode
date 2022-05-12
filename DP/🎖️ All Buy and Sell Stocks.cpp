4488c8fd937cc4942ea63272a73c85600a72aa5a
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-12 13:14:01 +0530

    Update 🎖️ All Buy and Sell Stocks.cpp

--- DP/🎖️ All Buy and Sell Stocks.cpp
+++ DP/🎖️ All Buy and Sell Stocks.cpp
@@ -26,3 +26,62 @@ int maxProfit(vector<int>& prices)
 
 
 
+█▄▄ █░█ █▄█   ▄▀█ █▄░█ █▀▄   █▀ █▀▀ █░░ █░░   █▀ ▀█▀ █▀█ █▀▀ █▄▀   ▄▄   █ █
+█▄█ █▄█ ░█░   █▀█ █░▀█ █▄▀   ▄█ ██▄ █▄▄ █▄▄   ▄█ ░█░ █▄█ █▄▄ █░█   ░░   █ █
+
+( Many transaction )
+        
+// LINK - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+// 🅣🅗🅔🅞🅡🅨 - see the photo uploaded
+
+DP solution of BUY AND SELL STOCK - 2 
+        
+𝓒𝓞𝓓𝓔 - 
+class Solution {
+    
+    int dp[30003][2];
+    
+    int helper(vector<int>& prices, int index, int buy)
+    {
+        if(index==prices.size())
+            return 0;
+        if(dp[index][buy] != -1)
+            return dp[index][buy];
+        int profit = INT_MIN;
+        if(buy)
+        {
+            profit = max(-prices[index]+helper(prices,index+1,0) , helper(prices,index+1,1));
+        }
+        else
+        {
+            profit = max(prices[index]+helper(prices,index+1,1) , helper(prices,index+1,0));
+        }
+        
+        return dp[index][buy]=profit;
+    }
+    
+public:
+    int maxProfit(vector<int>& prices) {
+      memset(dp,-1,sizeof(dp));
+        return helper(prices,0,1);
+    }
+};
+
+// VALLEY PEAK 
+𝓒𝓞𝓓𝓔 - 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+        int diff = 0;
+        for(int i=1;i<n;++i)
+        {
+            if(prices[i] > prices[i-1])
+                diff += prices[i]-prices[i-1];
+        }
+        
+        return diff;
+    }
+};
