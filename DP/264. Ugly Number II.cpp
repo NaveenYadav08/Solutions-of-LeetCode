bb36006c09592a3826192386c9b04c8ff541b49f
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-12 21:56:54 +0530

    Create 264. Ugly Number II.cpp

---
+++ DP/264. Ugly Number II.cpp
@@ -0,0 +1,27 @@
+// LINK https://leetcode.com/problems/ugly-number-ii/
+
+// CODE 
+
+class Solution {
+public:
+int nthUglyNumber(int n) {
+vector<int> dp(n);
+dp[0]=1;
+int p2=0;
+int p3=0;
+int p5=0;
+
+    for(int i=1;i<n;i++)
+    {
+        int multipletwo=dp[p2]*2;
+        int multiplethree=dp[p3]*3;
+        int multiplefive=dp[p5]*5;
+        
+        dp[i]=min(multipletwo,min(multiplethree,multiplefive));
+        if(dp[i]==multipletwo)p2++;
+        if(dp[i]==multiplethree)p3++;
+        if(dp[i]==multiplefive)p5++;
+    }
+    return dp[n-1];
+}
+};
