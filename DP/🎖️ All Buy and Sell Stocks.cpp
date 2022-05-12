abc8cd6ee6f59e801580e118e93d729697d2a1ca
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-12 09:13:10 +0530

    Update 🎖️ All Buy and Sell Stocks.cpp

--- DP/🎖️ All Buy and Sell Stocks.cpp
+++ DP/🎖️ All Buy and Sell Stocks.cpp
@@ -1,1 +1,27 @@
 
+█▄▄ █░█ █▄█   ▄▀█ █▄░█ █▀▄   █▀ █▀▀ █░░ █░░   █▀ ▀█▀ █▀█ █▀▀ █▄▀   ▄▄   █
+█▄█ █▄█ ░█░   █▀█ █░▀█ █▄▀   ▄█ ██▄ █▄▄ █▄▄   ▄█ ░█░ █▄█ █▄▄ █░█   ░░   █
+
+// LINK - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// 🅣🅗🅔🅞🅡🅨
+
+// For selling the stock on ith day I will buy it at 
+// min price ( 0 to i-1 th day )
+
+𝓒𝓞𝓓𝓔
+
+int maxProfit(vector<int>& prices) 
+{
+        int n = prices.size();
+        int maxprofit = 0;
+        int minvalue = prices[0];
+        for(int i=0;i<n;i++){
+            minvalue = min(prices[i],minvalue);
+            maxprofit= max(maxprofit,prices[i] - minvalue);
+        }
+        return maxprofit;
+}
+
+
+
