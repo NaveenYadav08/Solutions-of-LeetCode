1ac1f7d53b098354ae33a9c227e82163210d2fed
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-15 13:33:57 +0530

    Update 🎖️ 410. Split Array Largest Sum.cpp

--- DP/🎖️ 410. Split Array Largest Sum.cpp
+++ DP/🎖️ 410. Split Array Largest Sum.cpp
@@ -3,19 +3,25 @@
 
 class Solution {
 public:
-     const int MOD=1e9+7;
-    int dp[1002][52];
+    const int MOD=1e9+7;
+    int dp[1002][52]; 
+    // ans from index i to end  , m parts
     
     int f(int i,int m,vector<int>& nums){
-        if(i==nums.size()){
+        if(i==nums.size())
+        {
             return (m)? MOD:0;
         }
+        
         if(!m)return MOD;
+        
         if(m==1){
             int sum=accumulate(nums.begin()+i,nums.end(),0LL);
             return sum;
         }
+        
         if(dp[i][m]!=-1)return dp[i][m];
+      
         int ans=MOD;
         
         int sum=0;
@@ -26,8 +32,9 @@ public:
         return dp[i][m]=ans;
     }
     
-    int splitArray(vector<int>& nums, int m) {
+    int splitArray(vector<int>& nums, int m) 
+    {
          memset(dp,-1,sizeof(dp));
-        return f(0,m,nums);
+         return f(0,m,nums);
     }
 };
