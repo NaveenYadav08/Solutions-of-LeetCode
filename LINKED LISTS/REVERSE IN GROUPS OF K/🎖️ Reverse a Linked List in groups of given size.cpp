88b05814f94e7876272ffd670e2b088e65f54346
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-19 14:17:16 +0530

    Create 🎖️ Reverse a Linked List in groups of given size.cpp

---
+++ LINKED LISTS/REVERSE IN GROUPS OF K/🎖️ Reverse a Linked List in groups of given size.cpp
@@ -0,0 +1,79 @@
+// IT HAS 2 TYPES 
+
+
+
+1 /// rev the left outs too   https://practice.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+  
+  2 /// leave the left outs   https://leetcode.com/problems/reverse-nodes-in-k-group/
+  
+  
+  
+  
+  TYPE 1 
+  
+  struct node *reverse (struct node *head, int k)
+    { 
+        // Complete this method
+        node *curr=head;
+        node *prev=NULL;
+        node *next=NULL;
+        int count=0;
+        while(curr!=NULL and count<k)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            count++;
+        }
+        
+        if(next!=NULL)
+        {
+            head->next=reverse(next,k);
+        }
+        return prev;
+    }
+
+
+TYPE 2 
+  
+ ListNode* reverseKGroup(ListNode* head, int k) {
+    
+int count=0;
+ListNode* temp=head;
+    
+while(temp!=NULL)
+{
+count++;
+temp=temp->next;
+}
+
+    ListNode* curr=head;
+    ListNode* prev=NULL;
+    ListNode* next;int cnt=0;
+    while(curr!=NULL && cnt<k)
+    {
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        cnt++;
+    }
+    
+    if(next!=NULL && (count-k)>=k)
+    {
+        head->next=reverseKGroup(curr,k);
+    }
+    else
+    {
+        head->next=curr;
+    }
+    return prev;
+}
+  
+  
+  
+  
+  
+
+
