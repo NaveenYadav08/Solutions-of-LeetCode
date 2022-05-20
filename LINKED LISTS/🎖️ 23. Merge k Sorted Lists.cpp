59a9d90371d79ecb301629812b707cebeabf11ff
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-20 11:20:10 +0530

    Create 🎖️ 23. Merge k Sorted Lists.cpp

---
+++ LINKED LISTS/🎖️ 23. Merge k Sorted Lists.cpp
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/merge-k-sorted-lists/
+/// 
+
+class Solution {
+public:
+    
+    ListNode* merge(ListNode *a, ListNode *b)
+{
+    if(a==NULL) return b;
+    if(b==NULL) return a;
+    
+    ListNode *r;
+    if(a->val < b->val)
+    {
+        r=a;
+        r->next=merge(a->next,b);
+    }
+    else
+    {
+        r=b;
+        r->next=merge(a,b->next);
+    }
+    
+    return r;
+}
+    
+int n; 
+vector<ListNode*> LIST;
+
+ListNode* ans(int i)
+{    
+    if(i+1==n) return LIST[i];
+   
+   return merge(LIST[i], ans(i+1));
+}
+
+
+ListNode *mergeKLists(vector<ListNode*>& lists)
+{
+   // Your code here
+   
+     n = lists.size();
+    
+    for(int i=0;i<n;i++)
+    {
+        if(lists[i]!=NULL)
+            LIST.push_back(lists[i]);
+    }
+     n=LIST.size();
+    if(n==0)
+        return NULL;
+     return ans(0);
+    
+}
+
+};
+
