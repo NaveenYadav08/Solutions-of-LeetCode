d6a615216bde6fe6023749d6004804446a019f2f
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-20 17:45:12 +0530

    Update 🎖️ Largest Number.cpp

--- ARRAYS/🎖️ Largest Number.cpp
+++ ARRAYS/🎖️ Largest Number.cpp
@@ -1,58 +1,44 @@
 // https://www.interviewbit.com/problems/largest-number/
 
-// 
+
+// euailze digits and sorrt then 
 
 
 
-// euailze digits and sorrt then if same like 3 , 30 put 3 before
-
-bool compareT(int A,int B)
-{
-    int dA= floor(log10(A)+1);
-    int dB=floor(log10(B)+1);
-    
-    int cA=A;
-    int cB=B;
-    
-    if(dA<dB)
-    {
-        int diff=dB-dA;
-        cA=cA*(pow(10,diff));
-    }
-    else if(dA>dB)
-    {
-        int diff=dA-dB;
-        cB=cB*(pow(10,diff));  
-        
-    }
-
-    // EQUALIZE DIGITS
-    
-    if(cA>cB)
-    return true;
-    
-    else if(cA==cB)
-    {
-        if(A<B)
-        return true;
-    }
-    
-    return false;
-}
+static bool compareT(string a, string b) {
+            return a + b > b + a;
+        }
 
 string Solution::largestNumber(const vector<int> &A) {
     
-    vector<int> KA=A;
+    vector<string> KA;
+    for(int i=0;i<A.size();i++)
+    {
+        KA.push_back(to_string(A[i]));
+    }
     sort(KA.begin(),KA.end(),compareT);
     
     string AA="";
-    for(int i=0;i<A.size();i++)
+    for(int i=0;i<KA.size();i++)
     {
-        AA+=to_string(KA[i]);
+        AA+=KA[i];
     }
-    if(stoi(AA)==0)
+    
+    int flag=0;
+    for(int i=0;i<AA.size();i++)
     {
-        return "0";
+        if(AA[i]!='0')
+        {
+            flag=1;
+            break;
+        }
     }
+    
+    if(flag==0)
+    return "0";
+    
     return AA;
 }
+
+
+
