3c1a4d06bc688a6d550520c659348d96368efcc4
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-23 09:07:36 +0530

    Create 🎖️  363. Max Sum of Rectangle No Larger Than K.cpp

---
+++ DP/🎖️  363. Max Sum of Rectangle No Larger Than K.cpp
@@ -0,0 +1,40 @@
+https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
+
+https://www.youtube.com/watch?v=VsBXWpBbuig
+
+
+prefix Sum & set for lookup
+
+class Solution {
+public:
+    int kadane(vector<int>  nums,int K){
+        int result = INT_MIN;
+        int sum=0;
+        set<int> s;
+        s.insert(sum);
+        for(int num : nums){
+            sum+=num;
+            auto tmp = s.lower_bound(sum-K);
+            if(tmp != s.end()) result = max(result, sum- *tmp);
+            s.insert(sum);
+        }
+        return result;
+    }
+    
+    
+
+    
+    
+    int maxSumSubmatrix(vector<vector<int>>& a, int k) {
+        int ans=INT_MIN;
+        for(int i=0;i<a.size();i++){
+            vector<int> v(a[0].size(),0);
+            for(int j=i;j<a.size();j++){
+                for(int k=0;k<a[0].size();k++)
+                    v[k]+=a[j][k];
+                ans=max(ans,kadane(v,k));
+            }
+        }
+        return ans;
+    }
+};
