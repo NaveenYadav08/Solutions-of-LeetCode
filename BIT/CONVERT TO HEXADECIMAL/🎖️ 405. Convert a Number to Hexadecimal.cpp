795e5a92aadf19ed63b94b40e7f2cd2c4449a1a4
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-28 04:04:30 +0530

    Create 🎖️ 405. Convert a Number to Hexadecimal.cpp

---
+++ BIT/CONVERT TO HEXADECIMAL/🎖️ 405. Convert a Number to Hexadecimal.cpp
@@ -0,0 +1,19 @@
+https://leetcode.com/problems/convert-a-number-to-hexadecimal/
+
+class Solution {
+public:
+    string toHex(int num) {
+        unsigned int x = num;
+        string ans;
+        string hexaDec = "0123456789abcdef";
+        if (x==0){
+        return "0";
+        }
+        while(x > 0){
+            ans += hexaDec[x%16];
+            x/=16;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
