219bda57add394048b50097e01de3ec77d0c9806
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-05-31 16:53:22 +0530

    Update _M Product of array except itself.cpp

--- ARRAYS/_M Product of array except itself.cpp
+++ ARRAYS/_M Product of array except itself.cpp
@@ -28,3 +28,28 @@ public:
 
 
 // TC :: O(n) and SC :: O(1)
+
+
+// if I can reuse divide 
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int flag=0,mul=1;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0) mul=mul*nums[i];
+            else flag++;
+        }
+        for(int i=0;i<n;i++){
+            if(flag==1){
+                if(nums[i]==0) nums[i]=mul;
+                else nums[i]=0;
+            }
+            if(flag==0 && nums[i]!=0) nums[i]=mul/nums[i];
+            if(flag>1 ) nums[i]=0;
+           
+        }
+        return nums;
+    }
+};
