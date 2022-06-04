d3816c5f672c9c7255deccb37dda297fe6d32d64
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-04 17:20:08 +0530

    Update code.cpp

--- TREES/DELETE , FIND , INSERT IN BST/code.cpp
+++ TREES/DELETE , FIND , INSERT IN BST/code.cpp
@@ -46,10 +46,13 @@ public:
     }
     
     TreeNode* deleteNode(TreeNode* root, int key) {
+        
         if(root==nullptr) return root;
         else if(root->val>key) root->left=deleteNode(root->left,key);
         else if(root->val<key) root->right=deleteNode(root->right,key);
         else{
+            if(root->left==NULL and root->right==NULL)
+                return NULL;
             if(root->right==nullptr){
                 return root->left;
             }else if(root->left==nullptr){
@@ -63,5 +66,6 @@ public:
         return root;
     }
    
+};
 };
 
