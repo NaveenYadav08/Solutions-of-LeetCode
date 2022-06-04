cfbada3b846408a0d30f7d03dc4891c44c57a417
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-04 16:22:17 +0530

    Create 🎖️ 222 Count Complete Tree Nodes.cpp

---
+++ TREES/Count Complete Tree Nodes/🎖️ 222 Count Complete Tree Nodes.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/count-complete-tree-nodes/
+
+
+
+class Solution {
+public:
+ int countNodes(TreeNode* root) { // o(n)
+        if(!root) return 0;
+    return countNodes2(root);
+        //return 1+countNodes(root->left)+countNodes(root->right);
+    }
+    
+    int countNodes2(TreeNode *root) { // log(n)
+        if(!root) return 0;
+        int left = 0, right = 0;
+        TreeNode *l = root; TreeNode *r = root;
+        while(l) l = l->left, left++;
+        while(r) r = r->right, right++;
+        if(left == right) return pow(2, left) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
+
