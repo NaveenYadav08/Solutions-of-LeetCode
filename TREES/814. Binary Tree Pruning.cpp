2a5ddb7a3dbd7e16a39c2b2a33ea6111116355bf
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-05 16:09:21 +0530

    Create 814. Binary Tree Pruning.cpp

---
+++ TREES/814. Binary Tree Pruning.cpp
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/binary-tree-pruning/
+
+class Solution {
+
+    // TC : O(n)
+    // SC : O(H = logn)
+    public:
+    
+    bool canDeleteNode(TreeNode* root){
+        if(root->left == NULL && root->right == NULL && root->val == 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    
+    TreeNode* pruneTree(TreeNode* root) {
+        if (root == NULL) return NULL;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+
+        if (canDeleteNode(root)) return NULL;
+        return root   ;
+    }
+
+};
