05c14f997240d49c7cef9692f77a53fa57ec3641
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-06 10:20:59 +0530

    Update 🎖️ Count the paths .cpp

--- GRAPHS/QUESTIONS/🎖️ Count the paths .cpp
+++ GRAPHS/QUESTIONS/🎖️ Count the paths .cpp
@@ -1,1 +1,25 @@
 https://practice.geeksforgeeks.org/problems/count-the-paths4332/1
+
+
+class Solution {
+public:
+	int count_dfs(vector<vector<int>>graph, int u, int d, vector<bool>&vis){
+		if(u == d)
+			return 1;
+		vis[u] = true;
+		int ans = 0;
+		for(auto v: graph[u]){
+			if(!vis[v])
+				ans += count_dfs(graph, v, d, vis);
+		}
+		vis[u] = false;
+		return ans;
+	}
+	int possible_paths(vector<vector<int>>edges, int n, int s, int d){
+		vector<vector<int>>graph(n);
+		for(auto i: edges)
+			graph[i[0]].push_back(i[1]);
+		vector<bool>vis(n, false);
+		return count_dfs(graph, s, d, vis);
+	}
+};
