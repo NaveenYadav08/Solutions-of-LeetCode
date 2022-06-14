2f91a659cbf6e06018d102a773c164945c37c138
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-14 12:33:14 +0530

    Update 🎖️ Min Stack.cpp

--- STACKS & QUEUES/🎖️ Min Stack.cpp
+++ STACKS & QUEUES/🎖️ Min Stack.cpp
@@ -4,47 +4,79 @@ https://leetcode.com/problems/min-stack/
 
 
 
-int arr[12345678],minimum[12345678];
-int cur,mincur;
-MinStack::MinStack() {
-    cur=-1;
-    mincur=-1;
-}
+class MinStack {
+public:
+  stack<pair<int, int>> minStack;
+  MinStack() {
+   
+  }
 
-void MinStack::push(int x) {
-    //assert(cur<=1234567);
-    if(mincur==-1 || x<=minimum[mincur])
-    {
-        mincur++;
-        minimum[mincur]=x;
-    }
-    cur++;
-    arr[cur]=x;
-}
+  void push(int val) {
+    int minimum = !minStack.empty()? min(val, minStack.top().second): val;
+    minStack.push({val, minimum});
+  }
 
-void MinStack::pop() {
-    //assert(cur<=1234567);
-    if(cur==-1)
-     return;
-    if(arr[cur]==minimum[mincur])
-     mincur--;
-    cur--;
-}
+  void pop() {
+    minStack.pop();
+  }
 
-int MinStack::top() {
-    //assert(cur<=1234567);
-    if(cur==-1)
-     return -1;
+  int top() {
+    return minStack.top().first;
+  }
+
+  int getMin() {
+    return minStack.top().second;
+  }
+};
+
+
+
+// BEAKR    GANDA SOLUTION 
+// int arr[12345678],minimum[12345678];
+// int cur,mincur;
+// MinStack::MinStack() {
+//     cur=-1;
+//     mincur=-1;
+// }
+
+// void MinStack::push(int x) {
+//     //assert(cur<=1234567);
+//     if(mincur==-1 || x<=minimum[mincur])
+//     {
+//         mincur++;
+//         minimum[mincur]=x;
+//     }
+//     cur++;
+//     arr[cur]=x;
+// }
+
+// void MinStack::pop() {
+//     //assert(cur<=1234567);
+//     if(cur==-1)
+//      return;
+//     if(arr[cur]==minimum[mincur])
+//      mincur--;
+//     cur--;
+// }
+
+// int MinStack::top() {
+//     //assert(cur<=1234567);
+//     if(cur==-1)
+//      return -1;
     
-    return arr[cur];
-}
+//     return arr[cur];
+// }
 
-int MinStack::getMin() {
-    //assert(cur<=1234567);
-    if(mincur==-1)
-     return -1;
-    return minimum[mincur];
-}
+// int MinStack::getMin() {
+//     //assert(cur<=1234567);
+//     if(mincur==-1)
+//      return -1;
+//     return minimum[mincur];
+// }
+
+
+
+
 
 
 
