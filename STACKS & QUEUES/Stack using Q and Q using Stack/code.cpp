359be8ecb24c029c035ecbb92a4efbbdb7ed1f47
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-14 10:55:45 +0530

    Update code.cpp

--- STACKS & QUEUES/Stack using Q and Q using Stack/code.cpp
+++ STACKS & QUEUES/Stack using Q and Q using Stack/code.cpp
@@ -84,3 +84,90 @@ public:
 --------------------------------------------------------------------------------------
   
   
+  https://leetcode.com/problems/implement-queue-using-stacks/
+
+one stack recursion
+
+class MyQueue {
+    stack<int> s1;
+    
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(empty()){
+            s1.push(x);
+            return ;
+        }
+        int top = pop();
+        push(x);
+        s1.push(top);
+       
+    }
+    
+    int pop() {
+        int top = s1.top();
+        s1.pop();
+        return top;
+    }
+    
+    int peek() {
+         int top = s1.top();
+        return top;
+    }
+    
+    bool empty() {
+        return s1.size()==0;
+    }
+};
+
+
+
+// two stack  
+
+class MyQueue {
+    stack<int> s1,s2;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        //Move all elements from s1 to s2
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
+        }
+        //push item into s1
+        s1.push(x);
+        //push everything back to s1
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
+        }
+        
+    }
+    
+    int pop() {
+        if(s1.empty()){
+            return -1;
+        }
+        int top=peek();
+        s1.pop();
+        return top;
+    }
+    
+    int peek() {
+        int t=s1.top();
+        return t;
+    }
+    
+    bool empty() {
+        if(s1.empty())
+            return true;
+        return false;
+    }
+};
+        
