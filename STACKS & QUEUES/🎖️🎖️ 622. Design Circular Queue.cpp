c2174d23cb182fea7ae2aa9213d37ba62c6b413e
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-15 19:52:35 +0530

    Create 🎖️🎖️ 622. Design Circular Queue.cpp

---
+++ STACKS & QUEUES/🎖️🎖️ 622. Design Circular Queue.cpp
@@ -0,0 +1,78 @@
+https://leetcode.com/problems/design-circular-queue/
+
+
+622. Design Circular Queue
+
+
+
+
+
+
+
+class MyCircularQueue {
+public:
+	int *arr;
+	int frontIndex;
+	int rearIndex;
+	int size;
+	int capacity;
+
+	MyCircularQueue(int k) {
+		arr = new int[k];
+		rearIndex = 0;
+		frontIndex = -1;
+		size = 0;
+		capacity = k;        
+	}
+
+	bool enQueue(int value) {
+		if(size == capacity) {
+			return false;
+		}
+		arr[rearIndex] = value;
+		rearIndex = (rearIndex + 1) % capacity ;
+		if(frontIndex == -1) {
+			frontIndex = 0;
+		}
+		size++;
+		return true;
+	}
+
+	bool deQueue() {
+		if(isEmpty()) {
+			return false;
+		}
+		frontIndex = (frontIndex + 1) % capacity;
+		size--;
+		if(size == 0) {
+			frontIndex = -1;
+			rearIndex = 0;
+		}
+		return true;        
+	}
+
+	int Front() {
+		if(isEmpty()) {
+			return -1;
+		}
+		return arr[frontIndex];        
+	}
+
+	int Rear() {
+		if(isEmpty()) {
+			return -1;
+		}
+		return arr[(rearIndex + capacity - 1) % capacity];
+	}
+
+	bool isEmpty() {
+		return size == 0;
+	}
+
+	bool isFull() {
+		if(size == capacity) {
+			return true;
+		}
+		return false;
+	}
+};
