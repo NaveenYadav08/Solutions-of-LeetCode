69125551a03a08db0bcb5f3eccb449640697cc44
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-17 23:32:02 +0530

    Update Largest Permutation.cpp

--- GREEDY/Largest Permutation.cpp
+++ GREEDY/Largest Permutation.cpp
@@ -16,9 +16,9 @@ vector<int> Solution::solve(vector<int> &A, int B) {
             return A;
         }
 
-        if (A[i]<n-i){
-            int j=m[n-i];
-            m[A[i]]=j;
+        if (A[i]<n-i){  // 1 < 4  curr , what curr should be
+            int j=m[n-i]; // wo no jo idhar hae wo kaha pr hae 
+            m[A[i]]=j; // swap them
             int temp=A[i];
             A[i]=A[j];
             A[j]=temp;
