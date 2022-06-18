18a1196373a436aea61176a657135a6623379b6f
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-18 06:27:54 +0530

    Update 🎖️ Distribute Candy.cpp

--- GREEDY/🎖️ Distribute Candy.cpp
+++ GREEDY/🎖️ Distribute Candy.cpp
@@ -23,3 +23,53 @@ public:
       return ans;
     }
 };
+
+
+// CONSTANT SPACE //
+class Solution {
+  public:
+    
+    int sum(int n) {
+            return (n * (n + 1)) / 2;
+    }
+    
+    int candy(vector<int>& ratings) {
+        if(ratings.size() <= 1) return ratings.size();
+        int candies = 0;
+        int up = 0, down = 0; 
+        int prevSlope = 0;
+        
+        for(int i = 1; i < ratings.size(); i++) {
+            //If increasing then 1; if decreasing then -1; if equal then 0.
+int currSlope = (ratings[i] > ratings[i-1]) ? 1  : (ratings[i] < ratings[i-1] ? -1 : 0);
+            //                                       _
+            //If mountain is changing. \_ || \/ ||  /
+            if((prevSlope < 0 && currSlope >= 0) || (prevSlope > 0 && currSlope == 0)) {  
+                candies = candies + sum(up) + sum(down) + max(up, down);
+                 cout<<i<<" DD "<<up<<" "<<down<<endl;
+                up = 0; 
+                down = 0;
+              
+            }
+            
+            //Add in up/down if slope is increasing or decreasing respectively. 
+            //If it is a plain, add a candy as it is the base case.
+            if(currSlope > 0) {
+                up++;
+            }
+            else if(currSlope < 0) {
+                down++;
+            }
+            else {
+                candies++;
+            }
+            
+            prevSlope = currSlope;
+        } 
+        
+        candies = candies + sum(up) + sum(down) + max(up, down) + 1;
+        return candies;
+    }
+    
+
+};
