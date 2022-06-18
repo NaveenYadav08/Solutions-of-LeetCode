dfcdc66b8856797e328be7dd78a432b626ffc163
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-19 01:53:00 +0530

    Update 313. Super Ugly Number.cpp

--- HEAP/Super Ugly Number/313. Super Ugly Number.cpp
+++ HEAP/Super Ugly Number/313. Super Ugly Number.cpp
@@ -1,1 +1,39 @@
+https://leetcode.com/problems/super-ugly-number/
 
+
+   class Solution {
+    public:
+        using P = pair<long long, int>;
+       
+        int nthSuperUglyNumber(int n, vector<int>& primes) {
+            
+            vector<int> index(primes.size(), 0);
+            vector<long long> nums;
+            
+            nums.push_back(1);      
+            
+            priority_queue<P, vector<P>, greater<P>> pq;  
+            // remember pair(the ugly number, who generated this ugly)
+            
+            for (int i = 0; i < primes.size(); i++)
+                pq.push({primes[i], i});   // initial ugly = prime  * 1
+            
+            while (nums.size() < n)
+            {
+                auto [value, id] = pq.top();    // get the smallest ugly
+                pq.pop();
+                
+                if (value != nums.back())       // check the dupe
+                    nums.push_back(value);
+                
+                index[id]++;                             
+// the prime generated this small ugly number should move forward to multiply next ugly number
+                pq.push({nums[index[id]] * primes[id], id});         
+// push the new ugly number to pq to participate sorting
+            }
+            
+        
+            
+            return nums.back();
+        }
+    };
