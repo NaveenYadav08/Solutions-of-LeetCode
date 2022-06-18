79303d35f5eff0f85ecf08aedc7a0841c55f4ba4
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-18 14:17:30 +0530

    Create Kth largest element in a stream.cpp

---
+++ HEAP/Kth largest element in a stream.cpp
@@ -0,0 +1,32 @@
+https://practice.geeksforgeeks.org/problems/kth-largest-element-in-a-stream2220/1#
+
+
+class Solution {
+  public:
+    vector<int> kthLargest(int k, int arr[], int n) {
+        
+        vector<int> ans(n);
+        
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(int i=0; i<n; i++)
+        {
+            if(pq.size() < k)
+                pq.push(arr[i]);
+            else
+            {
+                if(arr[i] > pq.top())
+    			{
+    				pq.pop();
+    				pq.push(arr[i]);
+    			}
+            }
+            if(pq.size() < k)
+                ans[i] = -1;
+            else
+                ans[i] = pq.top();
+        }
+        
+        return ans;
+    }
+};
