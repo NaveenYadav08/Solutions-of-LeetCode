9eb3f772f998eac93bc378ef10a9d39e130c5cb7
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-19 11:00:52 +0530

    Create 🎖️ 857. Minimum Cost to Hire K Workers.cpp

---
+++ HEAP/MIN COST TO HIRE WORKERS/🎖️ 857. Minimum Cost to Hire K Workers.cpp
@@ -0,0 +1,40 @@
+https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
+
+class Solution {
+public:
+
+double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+    vector<pair<double,int>> v;
+    int n=quality.size();
+    for(int i=0;i<n;i++){
+        double factor=wage[i]/(double)quality[i];
+        v.push_back({factor,quality[i]});
+    }
+    
+    sort(v.begin(),v.end());
+    
+    priority_queue<int> pq; // max heap to keep track of qualities of worker
+    
+    double ans=DBL_MAX;
+    int q=0;
+    for(int i=0;i<n;i++){
+        if(pq.size()<k){
+            pq.push(v[i].second);
+            q += v[i].second;
+        }else if(pq.size()==k){
+            if(pq.top()>v[i].second){
+                q -= pq.top();
+                pq.pop();
+                
+                pq.push(v[i].second);
+                q += v[i].second;
+            }
+        }
+        if(pq.size()==k){
+            double cost = q*v[i].first;
+            if(ans > cost) ans=cost;
+        }
+    }
+    return ans;
+}
+};
