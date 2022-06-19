e0f85d86f66bdbc9dfabb8a77dfe4de67ee19f0d
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-19 12:53:23 +0530

    Update 🎖️ 786. K-th Smallest Prime Fraction.cpp

--- HEAP/🎖️ 786. K-th Smallest Prime Fraction.cpp
+++ HEAP/🎖️ 786. K-th Smallest Prime Fraction.cpp
@@ -3,47 +3,32 @@ https://leetcode.com/problems/k-th-smallest-prime-fraction/
 
 class Solution {
 public:
-	vector<int> findCount(vector<int> &arr, double mid) {
-		int n = arr.size();
-		int num = arr[0];
-		int denom = arr[n - 1];
-
-		int i = 0; int cnt = 0;
-		for (int j = 1; j < n; j++) {
-			while (arr[i] <= mid * arr[j]) i++;
-			cnt += i;
-
-			if (i > 0 && arr[i - 1] * denom >= num *  arr[j]) {
-				num = arr[i - 1];
-				denom = arr[j];
-			}
-		}
-
-		return {cnt, num, denom};
-	}
-    
-	vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-
-		int n = arr.size();
-		double low = (arr[0] * 1.0) / arr[n - 1];
-		double high = 1;
-
-		if (k == 1) return {arr[0], arr[n - 1]};
-		vector<int> ans;
-
-		while (low < high) {
-			double mid = (low + high) / 2;
-			vector<int> cur = findCount(arr, mid);
-
-			if (cur[0] == k) {
-				return {cur[1], cur[2]};
-			}
-
-			else if (cur[0] > k) {
-				high = mid;
-			} else low = mid;
-		}
-
-		return {};
-	}
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        double l = 0, r = 1;
+        
+        while (l < r) {
+            double m = (l + r) / 2;
+            int numerator = 0, denominator = 0, count = 0;
+            
+            for (int i = 0; i < arr.size(); ++i) {
+                auto it = lower_bound(arr.begin(), arr.end(), m * arr[i]);
+                count += it - arr.begin();
+                
+                if (it != arr.begin() && numerator * arr[i] <= *(it - 1) * denominator) {
+                    numerator = *(it - 1);
+                    denominator = arr[i];
+                }
+            }
+            
+            if (count == k) {
+                return {numerator, denominator};
+            } else if (count < k) {
+                l = m;
+            } else {
+                r = m;
+            }
+        }
+        
+        return {0, 0};
+    }
 };
