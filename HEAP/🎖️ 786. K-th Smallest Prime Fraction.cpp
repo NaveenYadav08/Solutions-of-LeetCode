76efedb7ac666e597aafbdf34213dfe158c55bc8
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-19 13:09:43 +0530

    Update 🎖️ 786. K-th Smallest Prime Fraction.cpp

--- HEAP/🎖️ 786. K-th Smallest Prime Fraction.cpp
+++ HEAP/🎖️ 786. K-th Smallest Prime Fraction.cpp
@@ -1,34 +1,3 @@
 https://leetcode.com/problems/k-th-smallest-prime-fraction/
 786. K-th Smallest Prime Fraction
 
-class Solution {
-public:
-    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        double l = 0, r = 1;
-        
-        while (l < r) {
-            double m = (l + r) / 2;
-            int numerator = 0, denominator = 0, count = 0;
-            
-            for (int i = 0; i < arr.size(); ++i) {
-                auto it = lower_bound(arr.begin(), arr.end(), m * arr[i]);
-                count += it - arr.begin();
-                
-                if (it != arr.begin() && numerator * arr[i] <= *(it - 1) * denominator) {
-                    numerator = *(it - 1);
-                    denominator = arr[i];
-                }
-            }
-            
-            if (count == k) {
-                return {numerator, denominator};
-            } else if (count < k) {
-                l = m;
-            } else {
-                r = m;
-            }
-        }
-        
-        return {0, 0};
-    }
-};
