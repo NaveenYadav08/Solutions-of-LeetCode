ecc2cb8e8652e4b11f5b073b1d5a498de58c2234
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-21 15:58:25 +0530

    Update 🎖️ 600. Non-negative Integers without Consecutive Ones.cpp

--- DP/Non-negative Integers without Consecutive Ones/🎖️ 600. Non-negative Integers without Consecutive Ones.cpp
+++ DP/Non-negative Integers without Consecutive Ones/🎖️ 600. Non-negative Integers without Consecutive Ones.cpp
@@ -2,6 +2,19 @@
 // 600. Non-negative Integers without Consecutive Ones
 // https://leetcode.com/problems/non-negative-integers-without-consecutive-ones/
 
+// fibonacci f[3] means n cons ones in bit represtation of length 3
+
+// now for x = 150 BITS = 10010110 , 8 bits here 
+// so MSB 8 = F( 7 ) = so it is all ans of 7 bit length and making 8th bit as 0 
+// next msb at 5 = F( 4 ) = mmaing 8th bit as 1 and all ans of length 4 making the 5th bit as 0 
+// Why it is so bcz at each time I have to make some bit 0 bcz it not done so I may get ans >= x 
+// now for MSB( 3 ) = F ( 2 ) = making 8th bit as set and All ans of len 2 making 3rd bit as 0 
+// now for MSB( 2 ) = F( 1 ) =  making 8th bit as 1 and ALL ans of len 1 making 2nd bit as 0 , 
+// as prev bit is 1 so return ans
+
+
+
+
 
 class Solution {
   public:
