316faa7891353b84d5a3693c0a8d2a0f81cff732
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-06-27 11:55:54 +0530

    Update INSERT , SEARCH IN TRIE.cpp

--- TRIE/INSERT , SEARCH IN TRIE.cpp
+++ TRIE/INSERT , SEARCH IN TRIE.cpp
@@ -53,10 +53,8 @@ If the reference trie for a character is not present return false else transvers
 	
 During delete operation we delete the key in bottom up manner using recursion. The following are possible conditions when deleting key from trie, 
 
-Key may not be there in trie. Delete operation should not modify trie.
-Key present as unique key (no part of key contains another key (prefix), nor the key itself is prefix of another key in trie). Delete all the nodes.
-Key is prefix key of another long key in trie. Unmark the leaf node.
-Key present in trie, having atleast one other key as prefix key. Delete nodes from end of key until first leaf node of longest prefix key.
+delete recursively 
+if no child and end of word del it
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
