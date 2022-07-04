be03aad9a9b93d58e0562d62d3013519579cd734
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-07-04 13:05:56 +0530

    Update 🎖️🎖️ Egg Dropping Puzzle.cpp

--- DP/🎖️🎖️ Egg Dropping Puzzle.cpp
+++ DP/🎖️🎖️ Egg Dropping Puzzle.cpp
@@ -4,6 +4,28 @@ https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle-1587115620/1
 
 // k floor N eggs
 
+/*
+The basic idea is, we have to find the minimum number of attempts to find the 
+Threshold Floor, that means, above that floor the egg will break and below that 
+floor the egg will not break
+
+So, we have two choices
+1. Egg will break
+2. Egg will not break
+
+-> For the first case if the egg will break from a particular floor then we have to go below
+that floor
+-> For the second case if the egg will not break from a particular floor then we have to go
+above to find the threshold floor
+
+Base Conditions:
+1. It is given that egg the will not be 0 so no check for this in code
+2. If the egg will be 1 i.e. k = 1, So we need at minimum n moves to determine what the value
+    of floor is i.e n attempts.
+3. If the n is 0 then no floor 0 attempt.
+4. If the n is 1 then only 1 attempt.
+*/
+
 class Solution
 {
     public:
