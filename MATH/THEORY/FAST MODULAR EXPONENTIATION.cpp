4cfad1ad0aacf4a63ccda0724641fa3796eed89b
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-07-05 16:40:41 +0530

    Update FAST MODULAR EXPONENTIATION.cpp

--- MATH/THEORY/FAST MODULAR EXPONENTIATION.cpp
+++ MATH/THEORY/FAST MODULAR EXPONENTIATION.cpp
@@ -1,10 +1,10 @@
 Following is a recursive function to find (ab) % m in O(log2b) time,
 
-int power(int a, int b)
+int power(int a, int b, int m)
 {
     if(b == 0) return 1;
     a = a % m;
-    int temp = power(a, b/2);
+    int temp = power(a, b/2, m);
     if(b&1)
         return (((temp * temp) % m) * a) % m;
     else
