b61ade46817c25bd696db847d9f66bbf58d41e63
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-07-06 12:32:14 +0530

    Create Modular multiplicative inverse also theory.cpp

---
+++ MATH/THEORY/Modular multiplicative inverse also theory.cpp
@@ -0,0 +1,9 @@
+Given two integers ‘a’ and ‘m‘, find modular multiplicative inverse of ‘a’ under modulo ‘m’.
+  it means that find  b so that ( a * b ) % m = 1
+  
+The multiplicative inverse of “a modulo m” exists if and only if a and m are relatively prime (i.e., if gcd(a, m) = 1).
+  
+  see it in proofs 
+  
+  
+  
