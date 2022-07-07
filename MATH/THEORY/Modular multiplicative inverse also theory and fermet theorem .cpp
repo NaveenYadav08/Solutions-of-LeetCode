c1b5af393d23bdf2cd7640bb39b9fcb3aa67e8a0
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-07-07 11:12:48 +0530

    Update Modular multiplicative inverse also theory and fermet theorem .cpp

--- MATH/THEORY/Modular multiplicative inverse also theory and fermet theorem .cpp
+++ MATH/THEORY/Modular multiplicative inverse also theory and fermet theorem .cpp
@@ -60,3 +60,78 @@ int gcdExtended(int a, int b, int& x, int& y)
 
 
 Time Complexity: O(log m)
+  
+  
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  
+  Method 3 FERMET'S LITTLE THEOREM 
+  (Works when m is prime) 
+  and a ia positive integer  and is not divisible by m
+If we know m is prime, then we can also use Fermats’s little theorem to find the inverse. 
+  
+  so basically if m dont divide a 
+
+a^(m-1) ≅ 1 (mod m)
+If we multiply both sides with a-1, we get 
+
+a^(-1) ≅ a^ (m-2) (mod m)
+  
+  
+  Time Complexity: O(log m)
+    
+    
+    CODE 
+// To find GCD of a and b
+int gcd(int a, int b);
+ 
+// To compute x raised to power y under modulo m
+int power(int x, unsigned int y, unsigned int m);
+ 
+// Function to find modular inverse of a under modulo m
+// Assumption: m is prime
+void modInverse(int a, int m)
+{
+    int g = gcd(a, m);
+    if (g != 1)
+        cout << "Inverse doesn't exist";
+    else
+    {
+        // If a and m are relatively prime, then modulo
+        // inverse is a^(m-2) mode m
+        cout << "Modular multiplicative inverse is "
+             << power(a, m - 2, m);
+    }
+}
+ 
+// To compute x^y under modulo m
+int power(int x, unsigned int y, unsigned int m)
+{
+    if (y == 0)
+        return 1;
+    int p = power(x, y / 2, m) % m;
+    p = (p * p) % m;
+ 
+    return (y % 2 == 0) ? p : (x * p) % m;
+}
+ 
+// Function to return gcd of a and b
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+ 
+// Driver code
+int main()
+{
+    int a = 3, m = 11;
+ 
+    // Function call
+    modInverse(a, m);
+    return 0;
+}
+    
+    
+ 
