5dea08559209760f7fc6cf3b11fde8c96c7d1139
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-07-17 14:52:01 +0530

    Update 🎖️ 139. Word Break.cpp

--- DP/🎖️ 139. Word Break.cpp
+++ DP/🎖️ 139. Word Break.cpp
@@ -2,6 +2,48 @@
 
 // CODE -
 
+// MEMOISED 
+
+class Solution {
+private:
+    vector<int> dp;
+    bool f(string s, int start, int n, vector<string>& wordDict, set<string> &st){
+        if(start == n) return true;
+        if(dp[start] != -1) return dp[start]; 
+        for(int i = start ;i < n; ++i){
+            if(st.count(s.substr(start, i - start + 1)) && f(s, i + 1, n, wordDict, st)) return dp[start] = true;
+        }
+        return dp[start] = false;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        set<string> st;
+        for(auto &e : wordDict){
+            st.insert(e);
+        }
+        dp.resize(n, -1);
+        return f(s, 0, n, wordDict, st);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
