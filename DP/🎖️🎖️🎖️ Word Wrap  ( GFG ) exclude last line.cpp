018f6fb6b2044287a09f8ca96134daa6422ac1e7
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-07-17 11:24:28 +0530

    Update 🎖️🎖️🎖️ Word Wrap  ( GFG ) exclude last line.cpp

--- DP/🎖️🎖️🎖️ Word Wrap  ( GFG ) exclude last line.cpp
+++ DP/🎖️🎖️🎖️ Word Wrap  ( GFG ) exclude last line.cpp
@@ -5,3 +5,32 @@ https://practice.geeksforgeeks.org/problems/word-wrap1646/1   SAME Q ONLY DIFF E
 
 CODE 
 
+
+class Solution {
+public:
+int wrapWord(int idx, vector<int> &nums, int k, vector<int> &dp) {
+        int n = nums.size();
+        if(idx == n) return 0;
+        
+        if(dp[idx] != -1) return dp[idx];
+        
+        int cost = INT_MAX, sum = 0;
+        for(int i = idx; i < n; i++) {
+            if(i > idx) sum++;
+            sum += nums[i]; if(sum > k) break;
+            if(i < n-1)
+                cost = min(cost, (k-sum) * (k-sum) + wrapWord(i+1, nums, k, dp));
+            else
+                cost = 0;
+        }
+        return dp[idx] = cost;
+    }
+
+    int solveWordWrap(vector<int>nums, int k) 
+    { 
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return wrapWord(0, nums, k, dp);
+    }
+};
+
