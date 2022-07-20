bb7fb015ce4ea60129a06e7a88f75c28b3372f91
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2022-07-20 15:29:08 +0530

    Create   🎖️🎖️ Minimum Swaps to Sort.cpp

---
+++ ARRAYS/🎖️🎖️ Minimum Swaps to Sort.cpp
@@ -0,0 +1,55 @@
+https://practice.geeksforgeeks.org/problems/minimum-swaps/1
+
+
+
+
+
+class Solution
+{
+    public:
+    //Function to find the minimum number of swaps required to sort the array. 
+	int minSwaps(vector<int>&nums)
+	{
+		int n = nums.size();
+		
+		//creating a list of pairs where first element of pair is array 
+        //element and second element is its position.
+		vector<pair<int,int>>cur(n);
+		
+		//iterating over the array elements.
+		for(int i = 0; i < n; i++)
+		{
+		    //storing the elements and their position as pair in the list.
+			cur[i].first = nums[i];
+			cur[i].second = i;
+		}
+		//sorting the list.
+		sort(cur.begin(), cur.end());
+		
+		//using boolean list to mark visited elements and currently 
+        //marking all the nodes as false.
+		vector<bool>vis(n, false);
+		int ans = 0;
+		
+		for(int i = 0; i < n; i++)
+		{
+		    //if element is already visited or it is already at
+		    //correct position, we continue the loop.
+			if(vis[i] or cur[i].second == i)
+				continue;
+			int cycle_size = 0;
+			int j = i;
+			
+			//while element is not visited, we find out the number of nodes in
+            //this cycle and keep incrementing cycle size.
+			while(!vis[j]){
+				vis[j] = true;
+				j = cur[j].second;
+				cycle_size++;
+			}
+			//updating number of swaps required.
+			ans += (cycle_size > 1) ? cycle_size - 1 : 0; 
+		}
+		return ans;
+	}
+};
