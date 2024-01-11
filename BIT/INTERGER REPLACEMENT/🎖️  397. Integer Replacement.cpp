9c7c391a21a7d8a3dbae5e911874d56ab4333d5b
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-01-11 22:57:59 +0530

    Update 🎖️  397. Integer Replacement.cpp

--- BIT/INTERGER REPLACEMENT/🎖️  397. Integer Replacement.cpp
+++ BIT/INTERGER REPLACEMENT/🎖️  397. Integer Replacement.cpp
@@ -21,3 +21,13 @@ public:
         return solve(n);
     }
 };
+
+
+/*-----------------IDEA---------------
+If number is odd
+    If number in 32-bit repesentaion is 'abccd....11'(rightmost 2nd bit is 1) then we add 1 because by adding one we will convert all righmost contineous 1 bits to 0, and more contineous rightmost bits be 0, we can get more contineous even numbers, so we can contineous keep dividing it by 2
+    
+    else if number in 32-bit repesentaion is 'abccd....01'(rightmost 2nd bit is 0) so we will subtract 1, so that most of right bits remains zero, so and more contineous rightmost bits be 0, we can get more contineous even numbers, so we can contineous keep dividing it by 2
+    
+ALTHOUGH 3 is exeptional because in bit representaion it is 00....011(2nd rightmost bit is one), but total number to make 3->1 increases if we add 1. So, we handle this case seprately
+*/
