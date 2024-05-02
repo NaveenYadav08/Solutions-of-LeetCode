313b92658141e3ab69197c5c6f2d1dad24fd2436
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-05-02 22:27:54 +0530

    Create Nth row.cpp

---
+++ ARRAYS/Pascal Triangle/Nth row.cpp
@@ -0,0 +1,13 @@
+void pascalTriangle(int n) {
+    long long ans = 1;
+    cout << ans << " "; // printing 1st element
+
+    //Printing the rest of the part:
+    for (int i = 1; i < n; i++) {
+        ans = ans * (n - i);
+        ans = ans / i;
+        cout << ans << " ";
+    }
+    cout << endl;
+}
+
