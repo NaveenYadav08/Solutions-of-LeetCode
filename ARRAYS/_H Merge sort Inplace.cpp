42df3700cd6426cc97c142dfbbc51af8405b537b
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-05-04 02:20:06 +0530

    Update _H Merge sort Inplace.cpp

--- ARRAYS/_H Merge sort Inplace.cpp
+++ ARRAYS/_H Merge sort Inplace.cpp
@@ -1,3 +1,36 @@
+// WAY ALPHA //
+    void mergeInPlace(vector<int>& nums,int start,int mid, int end) {
+        int total_len = end - start +1;
+        int gap = total_len/2 + total_len%2;
+        while(gap>0) {
+            int i = start, j = start + gap;
+            while(j<=end) {
+                if(nums[i]>nums[j]) {
+                    swap(nums[i], nums[j]);
+                }
+                i++,j++;
+            }
+            gap = gap<=1 ? 0 : gap/2 + gap%2;
+        }
+    }
+
+    void mergeSort(vector<int>& nums, int start, int end) {
+        if(start>=end) return;
+        int mid = (start+end)/2;
+        mergeSort(nums,start,mid);
+        mergeSort(nums,mid+1,end);
+        mergeInPlace(nums, start,mid,end);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size()-1);
+        return nums;
+    }
+
+
+-------------------------------------------------------------
+
+
 // WAY - 1
 // GAP METHOD
 // CODE 
