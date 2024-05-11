4ec054cc4f1cbeefefce3600f3d29abfa01b0b75
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-05-11 14:46:22 +0530

    Create  🎖️Nth root of M.cpp

---
+++ ARRAYS/🎖️Nth root of M.cpp
@@ -0,0 +1,30 @@
+Binary Search
+https://www.geeksforgeeks.org/problems/find-nth-root-of-m5843/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+//return 1, if == m:
+//return 0, if < m:
+//return 2, if > m:
+int func(int mid, int n, int m) {
+    long long ans = 1;
+    for (int i = 1; i <= n; i++) {
+        ans = ans * mid;
+        if (ans > m) return 2;
+    }
+    if (ans == m) return 1;
+    return 0;
+}
+
+int NthRoot(int n, int m) {
+    //Use Binary search on the answer space:
+    int low = 1, high = m;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int midN = func(mid, n, m);
+        if (midN == 1) {
+            return mid;
+        }
+        else if (midN == 0) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
