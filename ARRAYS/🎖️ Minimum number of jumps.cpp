2cff0f00b5dc41714c318077fba3113af7aaf130
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-06-02 17:52:50 +0530

    Create 🎖️ Minimum number of jumps.cpp

---
+++ ARRAYS/🎖️ Minimum number of jumps.cpp
@@ -0,0 +1,29 @@
+https://www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
+
+    int minJumps(int nums[], int n){
+        int currReach = 0;
+        // This variable keeps track of the maximum index (steps) that can be taken from the previous jump position..
+        int currMax = 0; // MaxJump ( all pssble betwwen prv jump pst and currReach )
+        int jumps = 0; // Final ANS
+        
+        for(int i=0;i<n-1; i++)
+        {
+            if(i+nums[i]>currMax)
+            {
+                currMax=i+nums[i]; 
+                // it will see all the jump between till currReach  from previous jump to see the max possibilty 
+            }
+            
+            if(i==currReach)
+            {// when I reach at currReach i will have to  take the jump and update new reach as maxreach 
+                jumps++;
+                currReach = currMax;
+            }
+
+        }
+        
+        if(currReach<n-1)
+        return -1;
+        
+        return jumps;
+    }
