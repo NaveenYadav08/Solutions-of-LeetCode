98f7f7415a6e7838b832cb31b08b75f53f2c98bb
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-06-02 17:54:46 +0530

    Update 🎖️ Minimum number of jumps.cpp

--- ARRAYS/🎖️ Minimum number of jumps.cpp
+++ ARRAYS/🎖️ Minimum number of jumps.cpp
@@ -1,5 +1,7 @@
 https://www.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
 
+https://leetcode.com/problems/jump-game-ii/
+
     int minJumps(int nums[], int n){
         int currReach = 0;
         // This variable keeps track of the maximum index (steps) that can be taken from the previous jump position..
