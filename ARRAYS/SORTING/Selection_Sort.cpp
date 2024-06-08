ce9e163afa7acc87ca0bf7c8ab91966eab05eed3
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-06-08 18:07:05 +0530

    Update Selection_Sort.cpp

--- ARRAYS/SORTING/Selection_Sort.cpp
+++ ARRAYS/SORTING/Selection_Sort.cpp
@@ -36,21 +36,3 @@ void selectionSort(int arr[], int n)
             swap(arr[min_idx], arr[i]);
     }
 }
-
-
-// RECURSIVE //
-void insertion_sort(int arr[], int i, int n) {
-
-    // Base Case: i == n.
-    if (i == n) return;
-
-    int j = i;
-    while (j > 0 && arr[j - 1] > arr[j]) {
-        int temp = arr[j - 1];
-        arr[j - 1] = arr[j];
-        arr[j] = temp;
-        j--;
-    }
-
-    insertion_sort(arr, i + 1, n);
-}
