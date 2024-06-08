1d815d6c600c0dd464019f0aa4d1f46ad5c8b221
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-06-08 18:34:29 +0530

    Delete ARRAYS/SORTING/QUICKSORT directory

--- ARRAYS/SORTING/QUICKSORT/Quick_sort.cpp
+++
@@ -1,3 +0,0 @@
-QuickSort is a sorting algorithm based on the Divide and Conquer algorithm that picks an 
-element as a pivot and partitions the given array around the picked pivot 
-by placing the pivot in its correct position in the sorted array.
