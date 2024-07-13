43dbde1140bb5109985abcd694164653f470978a
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-07-14 01:37:41 +0530

    Update 12 BIPARTITE DFS.cpp

--- GRAPHS/BASICS/12 BIPARTITE DFS.cpp
+++ GRAPHS/BASICS/12 BIPARTITE DFS.cpp
@@ -1,3 +1,8 @@
+A bipartite graph is a graph in which the vertices can be divided into two disjoint sets, 
+    such that no two vertices within the same set are adjacent. In other words, 
+it is a graph in which every edge connects a vertex of one set to a vertex of the other set.
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
