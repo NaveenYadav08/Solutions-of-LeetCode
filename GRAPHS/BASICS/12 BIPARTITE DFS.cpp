8d4946e743e5167a88c3b59c72aaa231f7076cc8
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-07-14 02:45:47 +0530

    Update 12 BIPARTITE DFS.cpp

--- GRAPHS/BASICS/12 BIPARTITE DFS.cpp
+++ GRAPHS/BASICS/12 BIPARTITE DFS.cpp
@@ -86,3 +86,53 @@ else
 cout<<"NO"<<endl;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////// LEETCODE ///////////////////////////////
+class Solution {
+    bool dfs(int node, int col, int color[], vector<vector<int>> adj) {
+        color[node] = col; 
+        
+        // traverse adjacent nodes
+        for(auto it : adj[node]) {
+            // if uncoloured
+            if(color[it] == -1) {
+                if(dfs(it, !col, color, adj) == false) return false; 
+            }
+            // if previously coloured and have the same colour
+            else if(color[it] == color[node]) {
+                return false; 
+            }
+        }
+        
+        return true; 
+    }
+public:
+	bool isBipartite(vector<vector<int>> adj){
+        int V = adj.size();
+	    int color[V];
+	    for(int i = 0;i<V;i++) color[i] = -1; 
+	    
+	    // for connected components
+	    for(int i = 0;i<V;i++) {
+	        if(color[i] == -1) {
+	            if(dfs(i, 0, color, adj) == false) 
+	                return false; 
+	        }
+	    }
+	    return true; 
+	}
+
+};
+
+
+
