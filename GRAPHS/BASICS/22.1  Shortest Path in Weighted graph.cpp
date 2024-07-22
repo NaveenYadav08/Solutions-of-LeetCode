20132945c5529f43a265a51f505fe667bb5b366b
Naveen_Yadav <66903732+NaveenYadav08@users.noreply.github.com>
2024-07-22 20:29:53 +0530

    Update 22.1  Shortest Path in Weighted graph.cpp

--- GRAPHS/BASICS/22.1  Shortest Path in Weighted graph.cpp
+++ GRAPHS/BASICS/22.1  Shortest Path in Weighted graph.cpp
@@ -1,48 +1,53 @@
 https://www.geeksforgeeks.org/problems/shortest-path-in-weighted-undirected-graph/1
 
-class Solution {
-  public:
-    vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
-        // Code here
-        vector<int> ans;
-        vector<int> parent(n + 1);
-        set<pair<int, pair<int, int>>> st;
-        st.insert({0, {1, 1}});
-        vector<int> vis(n + 1);
-        vector<vector<vector<int>>> adj(n + 1);
-        for (auto i : edges) {
-            adj[i[0]].push_back({i[1], i[2]});
-            adj[i[1]].push_back({i[0], i[2]});
-        }
-        int temp = -1;
-        while (!st.empty()) {
-            int d = st.begin()->first;
-            int p = st.begin()->second.first;
-            int par = st.begin()->second.second;
-            st.erase(st.begin());
-            if (vis[p] == 1)
-                continue;
-            parent[p] = par;
-            if (p == n) {
-                temp = d;
-                break;
-            }
-            vis[p] = 1;
-            for (auto j : adj[p]) {
-                st.insert({j[1] + d, {j[0], p}});
-            }
-        }
+// WORK FOR NON NEGATIVE 
+// CAN FIND ALL PATHS ALSO 
 
-        if (temp == -1)
-            return {-1};
-        int p = n;
-        while (p != 1) {
-            ans.push_back(p);
-            p = parent[p];
+
+    vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(auto i:edges){
+            int weight = i[2];
+            adj[i[0]].push_back({i[1],i[2]});
+            adj[i[1]].push_back({i[0],i[2]});
         }
-        ans.push_back(1);
-        ans.push_back(temp);
-        reverse(ans.begin(), ans.end());
+        
+        vector<int> distance(n+1,INT_MAX);
+        vector<int> parent(n+1,-1);
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q; // MIN HEAP //
+        
+        distance[1] = 0;
+        parent[1] = 0;
+        q.push({0,1});
+        
+        
+        while(!q.empty()){
+            int cur = q.top().second;
+            int d = q.top().first;
+            q.pop();
+            
+            for(auto k: adj[cur]){
+                int neighbour = k.first;
+                int weight = k.second;
+                
+                if(d + weight < distance[neighbour]){
+                    distance[neighbour] = d + weight;
+                    parent[neighbour] = cur;
+                    q.push({distance[neighbour],neighbour});
+                }
+            }
+        }
+        
+        if(distance[n] == INT_MAX) return {-1};
+        
+        vector<int> ans;
+        int temp = n;
+        while(temp != 0){
+            ans.push_back(temp);
+            temp = parent[temp];
+        }
+        ans.push_back(distance[n]);
+        reverse(ans.begin(),ans.end());
         return ans;
     }
-};
